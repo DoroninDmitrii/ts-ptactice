5bfaf58cb9a56aa001064a638e3159608ad40ee4
Dmitrii Doronin <doronin_dima@mail.ru>
2022-11-01 08:14:56 +0300

    enums

--- Tuples and Enums/tuplesAndEnums.ts
+++ Tuples and Enums/tuplesAndEnums.ts
@@ -15,3 +15,25 @@ const responses: HTTPResponse[] = [
   [200, "OK"],
 ];
 
+// Enum Example:
+enum OrderStatus {
+  PENDING,
+  SHIPPED,
+  DELIVERED,
+  RETURNED,
+}
+const myStatus = OrderStatus.DELIVERED;
+
+function isDelivered(status: OrderStatus) {
+  return status === OrderStatus.DELIVERED;
+}
+
+isDelivered(OrderStatus.RETURNED);
+
+// String Enum:
+enum ArrowKeys {
+  UP = "up",
+  DOWN = "down",
+  LEFT = "left",
+  RIGHT = "right",
+}
